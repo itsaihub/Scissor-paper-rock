6c83ad148809e8e34a8701986a00d3c60c3b637d
itsaihub <52753536+itsaihub@users.noreply.github.com>
2019-07-17 21:36:16 +0545

    updates

--- game.py
+++ game.py
@@ -1,95 +1,86 @@
 import random
 import tkinter as tk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 
 
+# INIT window, along with Title.
+window = tk.Tk()
+window.geometry("290x500")
+window.title("Scissor Paper Rock")
 
-window=tk.Tk()
-window.geometry("300x500")
-window.title("Scissor Paper Rock @Diwas ")
+# INIT IMG @ window.
+image = Image.open('aa.jpg')
+image.thumbnail((290, 290), Image.ANTIALIAS)
+photo = ImageTk.PhotoImage(image)
+label_image = tk.Label(image=photo)
+label_image.grid(column=15, row=0)
 
-image=Image.open('aa.jpg')
-image.thumbnail((300,300),Image.ANTIALIAS)
-photo=ImageTk.PhotoImage(image)
-label_image=tk.Label(image=photo)
-label_image.grid(column=15,row=0)
-
-#global variables
-USER_SCORE=0
-COMP_SCORE=0
-USER_CHOICE=""
-COMP_CHOICE=""
+# global variables
+RPS = {'scissor': 0, 'paper': 1, 'rock': 2}
+USER_SCORE = 0
+CPU_SCORE = 0
 
 
-
-def choice_to_number(choice):
-    rps={'scissor':0,'paper':1,'rock':2}
-    return rps[choice]
-
-def number_to_choice(number):
-        rps={0:'scissor',1:'paper',2:'rock'}
-        return rps[number]
-    
-def random_computer_choice():
-    return random.choice(['scissor','paper','rock'])
-
-def result(human_choice,comp_choice):
+# The main function.
+def function(user_choice, cpu_choice):
     global USER_SCORE
-    global COMP_SCORE
+    global CPU_SCORE
 
-    user=choice_to_number(human_choice)
-    comp=choice_to_number(comp_choice)
+    user = RPS[user_choice.lower()]
+    comp = RPS[cpu_choice.lower()]
 
-    if(user==comp):
-        print("Tie")
-    elif((user-comp)%3==1):
-         print("Sorry !! Com win")
-         USER_SCORE+=1
+    if(user == comp):
+        print("Tie.")
+    elif((user-comp) % 3 == 1):
+        print("Sorry! CPU won.")
+        CPU_SCORE += 1
     else:
-        print("Congarts !! You win")
-        COMP_SCORE+=1
-       
+        print("Congarts!! YOU won.")
+        
+        USER_SCORE += 1
 
-    #Text
-    text_area=tk.Text(master=window,height=12,width=30)
-    text_area.grid(column=15,row=4)
-    answer="Your Choice: {uc} \nComputer's Choice : {cc} \n Your Score : {u} \n Computer Score : {c}  \n\n made by diwas pandey ".format(uc=USER_CHOICE,cc=COMP_CHOICE,u=USER_SCORE,c=COMP_SCORE, font=('arial',24,'bold'))
-    text_area.insert(tk.END,answer)
+    # Text
+    text_area = tk.Text(master=window, height=12, width=30)
+    text_area.grid(column=15, row=4)
+    answer = 'You: {}\n'.format(user_choice)
+    answer += "CPU: {}\n".format(cpu_choice)
+    answer += '-'*30+'\n'
+    answer += "Your's Score: {}\n".format(USER_SCORE)
+    answer += "CPU's Score:  {}".format(CPU_SCORE)
+    text_area.insert(tk.END, answer)
 
 
-#Event Handling
+# Event Handling
+def event(user_choice):
+    cpu_choice = random.choice(list(RPS.keys()))
+    function(user_choice, cpu_choice)
+
+
 def rock():
-    global USER_CHOICE
-    global COMP_CHOICE
-    
-    USER_CHOICE='rock'
-    COMP_CHOICE=random_computer_choice()
-    result(USER_CHOICE,COMP_CHOICE)
+    event('Rock')
+
 
 def paper():
-    global USER_CHOICE
-    global COMP_CHOICE
-    
-    USER_CHOICE='paper'
-    COMP_CHOICE=random_computer_choice()
-    result(USER_CHOICE,COMP_CHOICE)
+    event('Paper')
+
 
 def scissor():
-    global USER_CHOICE
-    global COMP_CHOICE
-    
-    USER_CHOICE='scissor'
-    COMP_CHOICE=random_computer_choice()
-    result(USER_CHOICE,COMP_CHOICE)
-    
-#buttons
-button1=tk.Button(text="       Scissor         ",bg="red",command=scissor, height=1,width=8,font=('arial',15,'bold'))
-button1.grid(column=15,row=1)
-button2=tk.Button(text="        Paper          ",bg="pink",command=paper, height=1,width=8,font=('arial',15,'bold'))
-button2.grid(column=15,row=2)
-button3=tk.Button(text="         Rock          ",bg="yellow",command=rock, height=1,width=8,font=('arial',15,'bold'))
-button3.grid(column=15,row=3)  
+    event('Scissor')
 
+
+# Buttons
+button1 = tk.Button(text="Scissor", bg="#81CDDA", fg='#FFFFFF',
+                    command=scissor, height=1, width=8,
+                    font=('arial', 15, 'bold'))
+button1.grid(column=15, row=1)
+button2 = tk.Button(text="Paper", bg="#FFDF4C", fg='#FFFFFF',
+                    command=paper, height=1, width=8,
+                    font=('arial', 15, 'bold',))
+button2.grid(column=15, row=2)
+button3 = tk.Button(text="Rock", bg="#F5527B", fg='#FFFFFF',
+                    command=rock, height=1, width=8,
+                    font=('arial', 15, 'bold'))
+button3.grid(column=15, row=3)
 
 
 window.mainloop()
